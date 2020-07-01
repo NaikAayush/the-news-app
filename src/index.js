5c0a92b00b101127d7ef300818a76d97787d04e0
Aayush Naik <ayush@pesu.pes.edu>
2020-07-01 22:26:58 +0530

    final-setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqq_u4-z0JQi3_tBowkMzXHfRmnkE1fwc",
+  authDomain: "rip-news.firebaseapp.com",
+  databaseURL: "https://rip-news.firebaseio.com",
+  projectId: "rip-news",
+  storageBucket: "rip-news.appspot.com",
+  messagingSenderId: "886252853930",
+  appId: "1:886252853930:web:6df2a37cc37fbf859cced3"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
